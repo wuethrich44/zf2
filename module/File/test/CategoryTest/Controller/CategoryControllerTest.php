bc39dd2626c82918fe1c88d46d9a549fba1e28c3
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-06-21 01:01:59 +0200

    Remove Testcase

--- module/File/test/CategoryTest/Controller/CategoryControllerTest.php
+++ module/File/test/CategoryTest/Controller/CategoryControllerTest.php
@@ -45,10 +45,6 @@ class CategoryControllerTest extends AbstractHttpControllerTestCase {
                 ->method('fetchAll')
                 ->will($this->returnValue(array()));
 
-        $categoryTableMock->expects($this->any())
-                ->method('saveCategory')
-                ->will($this->returnValue(null));
-
         $serviceManager = $this->getApplicationServiceLocator();
         $serviceManager->setAllowOverride(true);
         $serviceManager->setService('File\Model\CategoryTable', $categoryTableMock);
@@ -66,21 +62,6 @@ class CategoryControllerTest extends AbstractHttpControllerTestCase {
         $this->assertControllerName('File\Controller\Category');
         $this->assertControllerClass('CategoryController');
         $this->assertMatchedRouteName('category');
-    }
-
-    public function testAddActionRedirectsAfterValidPost() {
-        $this->mockZfcLogin();
-        
-        $this->mockCategoryTable();
-
-        $postData = array(
-            'categoryID' => '',
-            'name' => 'Led Zeppelin',
-        );
-        $this->dispatch('/category/add', 'POST', $postData);
-        $this->assertResponseStatusCode(302);
-
-        $this->assertRedirectTo('/category/');
     }
 
 }
