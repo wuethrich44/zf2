25267403e15d4266f2c568e9ef39522af1490d7f
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-06-25 21:39:46 +0200

    Only register with HSLU-Mail

--- module/User/Module.php
+++ module/User/Module.php
@@ -2,6 +2,8 @@
 
 namespace User;
 
+use Zend\Filter\PregReplace;
+
 class Module {
 
     public function getConfig() {
@@ -16,6 +18,42 @@ class Module {
                 ),
             ),
         );
+    }
+
+    public function onBootstrap($e) {
+        $events = $e->getApplication()->getEventManager()->getSharedManager();
+
+        // Modify RegisterForm
+        $events->attach('ZfcUser\Form\Register', 'init', function($e) {
+            $form = $e->getTarget();
+            if ($form->has('username')) {
+                $form->get('username')->setAttribute('class', 'form-control');
+            }
+            if ($form->has('display_name')) {
+                $form->get('display_name')->setAttribute('class', 'form-control');
+            }
+            $form->get('email')->setAttribute('class', 'form-control');
+            $form->get('password')->setAttribute('class', 'form-control');
+            $form->get('passwordVerify')->setAttribute('class', 'form-control');
+            $form->get('submit')->setAttribute('class', 'btn btn-default');
+        });
+        
+        $events->attach('ZfcUser\Form\RegisterFilter', 'init', function($e) {
+            $filter = $e->getTarget();
+            // Attach @stud.hslu.ch at the end of the email
+            $filter->get('email')->getFilterChain()->attach(new PregReplace(array(
+                'pattern' => '/$/',
+                'replacement' => '@stud.hslu.ch',
+            )));
+        });
+
+        // Modify LoginForm
+        $events->attach('ZfcUser\Form\Login', 'init', function($e) {
+            $form = $e->getTarget();
+            $form->get('identity')->setAttribute('class', 'form-control');
+            $form->get('credential')->setAttribute('class', 'form-control');
+            $form->get('submit')->setAttribute('class', 'btn btn-default');
+        });
     }
 
 }
