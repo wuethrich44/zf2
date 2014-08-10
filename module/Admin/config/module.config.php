1574fb09b6a870b00dfb5a1b1499b7951c58de98
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-08-10 19:56:22 +0200

    Change admin layout

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -1,6 +1,11 @@
 <?php
 
 return array(
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view'
+        ),
+    ),
     'bjyauthorize' => array(
         'guards' => array(
             'BjyAuthorize\Guard\Controller' => array(
