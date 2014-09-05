f23d34b163e097a12a64d29a662081e7b745487c
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-09-05 11:04:49 +0200

    Refactor application configuration and enable caching

---
+++ config/application.config.development.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'modules' => array(
+        'EdpSuperluminal',
+    ),
+    'module_listener_options' => array(
+        'check_dependencies' => true,
+    ),
+);
--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,14 +11,28 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
-// provided by webserver
-$env = getenv('APP_ENV') ?: 'production';
+// provided from webserver
+$env = getenv('APP_ENV') ? : 'production';
 
 return array(
     'module_config' => array(
         'upload_location' => __DIR__ . '/../../data/uploads',
         'max_file_size' => 100000000 // Bytes
+    ),
+    'db' => array(
+        'driver' => 'Pdo',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+        )
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
     ),
     'view_manager' => array(
         'display_not_found_reason' => ($env == 'development'),
