390128e2b40c5e9a2ee66471c40fd52474a63599
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-10-09 23:22:41 +0200

    PHPCS fixes for Application #33

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -5,21 +5,28 @@ namespace Application;
 use Zend\Mvc\MvcEvent;
 use Zend\Validator\AbstractValidator;
 
-class Module {
+class Module
+{
 
-    public function onBootstrap(MvcEvent $e) {
+    public function onBootstrap(MvcEvent $e)
+    {
         $translator = $e->getApplication()->getServiceManager()->get('translator');
         $translator->addTranslationFile(
-                'phpArray', __DIR__ . '/language/de/Zend_Validate.php', 'default', 'de_DE'
+            'phpArray',
+            __DIR__ . '/language/de/Zend_Validate.php',
+            'default',
+            'de_DE'
         );
         AbstractValidator::setDefaultTranslator($translator);
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function getAutoloaderConfig() {
+    public function getAutoloaderConfig()
+    {
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
@@ -29,5 +36,4 @@ class Module {
             )
         );
     }
-
 }
